baee9bdaf7f42b484637873e7bafab60a0a625aa
mitko501 <miko501@azet.sk>
2014-05-05 19:48:16 +0200

    Dokoncene nacitavanie do pola a zoznamu, testovane valgrindom

--- nacvik_zapoctu/main2.c
+++ nacvik_zapoctu/main2.c
@@ -33,6 +33,12 @@ char* getLine(FILE* f){
     buffer[i]='\0';
     return buffer;
 }
+
+
+
+
+
+
 /**
  * Funkcia ktora nacitava riadky do dvojrozmerneho nepravouhleho pola
  * @brief readLinesToArray
@@ -65,6 +71,81 @@ int readLinesToArray(FILE* f,char*** records){
     return i;
 }
 
+
+
+
+typedef
+struct node{
+    char* data;
+    struct node* pNext;
+}node;
+
+typedef
+struct list{
+    node* start;
+    node* end;
+}list;
+
+node* addNode(list* linkedList){ //funkcia prida novy uzol
+    node* newe= NULL;
+    newe= malloc(sizeof(node));
+    newe->pNext=NULL;
+    newe->data= NULL;
+    if(linkedList->start==NULL){
+        linkedList->start= newe;
+    }else{
+        linkedList->end->pNext= newe;
+    }
+    linkedList->end= newe;
+    return newe;
+}
+
+
+/**
+ * Funkcia ktora nacitava riadky do spojeneho zoznamu
+ * @brief readLinesToLinkedList
+ * @param f subor z ktoreho nacitava
+ * @param linkedList zoznam do ktoreho ma nacitavat
+ * @return pocet riadkov ktore nacitala
+ */
+int readLinesToLinkedList(FILE* f, list* linkedList){
+    char* read=NULL;
+
+    node* newe= NULL;
+    int i=0;
+    while((read=getLine(f))!=NULL){
+        newe= addNode(linkedList);
+        newe->data= malloc(strlen(read)+1);
+        strcpy(newe->data,read);
+        free(read);
+        i++;
+    }
+    free(read);
+    return i;
+}
+
+void freeArray(char** ar, int num){
+    for(int i=0;i<num;i++){
+        free(ar[i]);
+    }
+    free(ar);
+}
+
+void freeLinkedList(list* linkedList){
+    node* current= linkedList->start;
+        node* before;
+        free(current->data);
+        before= current;
+        while(current->pNext != NULL){
+            current= current->pNext;
+            free(current->data);
+            free(before);
+            before= current;
+        }
+        free(before);
+        free(linkedList);
+}
+
 int main(int argc, char** argv){
     if(argc!=3){//skontrolujeme vstupny pocet argumentov
         fprintf(stderr,"Zly pocet argumentov");
@@ -77,17 +158,40 @@ int main(int argc, char** argv){
         fprintf(stderr,"Nepodarilo sa otvorit subor");
         return 1;
     }
-    char** records= NULL;
+
+    /*Nacitavanie cez zoznam aj s vypisom a mazanim pamate*//*
+    list* records= NULL;
+    records= malloc(sizeof(list));
+    if(records == NULL){
+        fprintf(stderr,"Nedostatok pamate!\n");
+        return 0;
+    }
+    records->end= NULL;
+    records->start= NULL;
     int lines=0;
-    if((lines=readLinesToArray(stdin,&records))==0){
+    if((lines=readLinesToLinkedList(f,records))==0){
+        printf("ziadnny riadok\n");
         return 1;
     }
+    node* current= records->start;
+    printf("%s\n",current->data);
+    while(current->pNext!=NULL){
+        current=current->pNext;
+        printf("%s\n",current->data);
+    }
+    freeLinkedList(records);*/
+
+    /*nacitavanie cez pole aj s vypisom a mazanim pamate*/
+    char** records= NULL;
+    int lines=0;
+    if((lines=readLinesToArray(f,&records))==0){
+        printf("ziadnny riadok\n");
+        return 1;
+    }
+
     for(int i=0;i<lines;i++){
         printf("%s\n",records[i]);
     }
-    for(int i=0;i<lines;i++){
-        free(records[i]);
-    }
-    free(records);
+    freeArray(records,lines);
     return 0;
 }
